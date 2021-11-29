1f1f7d39370be1cc3edeafba36769e0d9e4df623
Hesham <hesham2go@outlook.com>
2021-11-29 17:23:04 -0500

    connect DB, schema

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 
-   
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -13,18 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri, 
+    // { useNewUrlParser: true, useCreateIndex: true }
+    );
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+ connection.once('open', () => {
+   console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+// const exercisesRouter = require('./routes/exercises');
+// const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+// app.use('/exercises', exercisesRouter);
+// app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
